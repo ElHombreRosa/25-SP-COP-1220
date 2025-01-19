20bdae2f6e54633d48ae6cd6a19c1215efc4eb6b
ElHombreRosa <gabrielrosado0326@gmail.com>
2025-01-19 17:50:10 -0500

    saving progress 1-2

--- Module 1/Assingment 1-2/Lyrics.c
+++ Module 1/Assingment 1-2/Lyrics.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(){
+    
+    //Unit Test #1
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+
+    //Unit Test #2
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+
+    //Unit Test #3
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+
+    //Unit Test #4
+    printf("");
+    printf("");
+    printf("");
+    printf("");
 
     return 0;
 }
