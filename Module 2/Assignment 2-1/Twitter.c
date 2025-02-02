bcdafe58d44c73745e3761d8466637f54fba5572
ElHombreRosa <gabrielrosado0326@gmail.com>
2025-02-02 12:12:20 -0500

    Twitter Assignment Done

--- Module 2/Assignment 2-1/Twitter.c
+++ Module 2/Assignment 2-1/Twitter.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
+#define message_length 140
 
+int main(void){
+    char user_message[150];
+
+    scanf("%149[^\n]", user_message);
+
+    if (strlen(user_message) <= message_length){
+        printf("Posted\n");
+    }   else {
+        printf("Rejected\n");
+    }
 
     return 0;
 }
