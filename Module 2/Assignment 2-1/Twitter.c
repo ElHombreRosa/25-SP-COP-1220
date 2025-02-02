183dcbe8a9fd4c5be4dd6062ca4fea95b9b46165
ElHombreRosa <gabrielrosado0326@gmail.com>
2025-02-02 12:17:05 -0500

    Add Comments to Twitter Code

--- Module 2/Assignment 2-1/Twitter.c
+++ Module 2/Assignment 2-1/Twitter.c
@@ -4,14 +4,14 @@
 #define message_length 140
 
 int main(void){
-    char user_message[150];
+    char user_message[150]; // allows slightly more than 140 
 
-    scanf("%149[^\n]", user_message);
+    scanf("%149[^\n]", user_message); // read users input , as well avoid overflow
 
-    if (strlen(user_message) <= message_length){
-        printf("Posted\n");
+    if (strlen(user_message) <= message_length){ // If user message is less than or equal to message length (140 char)
+        printf("Posted\n"); // post the message 
     }   else {
-        printf("Rejected\n");
+        printf("Rejected\n"); // over 140 char reject message
     }
 
     return 0;
