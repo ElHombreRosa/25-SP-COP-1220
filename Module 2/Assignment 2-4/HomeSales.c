f0e2b38ab4d379df8c155ff9d2f0bd3e84c72037
ElHombreRosa <gabrielrosado0326@gmail.com>
2025-02-05 13:30:47 -0500

    Added Comments , Completed

--- Module 2/Assignment 2-4/HomeSales.c
+++ Module 2/Assignment 2-4/HomeSales.c
@@ -6,28 +6,28 @@ int main(){
     double sales_amount;
     char salesperson;
 
-    while(1) {
+    while(1) { // Loop to keep taking user input till they exit with Z
         printf("Sales Person Initials: (D, E, F, & Z To End Application): ");
         scanf(" %c", &salesperson);
-        salesperson = toupper(salesperson);
+        salesperson = toupper(salesperson); // Convert lowercase letters to uppercase to keep consistency
 
-        if (salesperson == 'Z'){
+        if (salesperson == 'Z'){ // End loop from users input Z
             break;
         }
 
-        if(salesperson != 'D' && salesperson != 'E' && salesperson != 'F') {
+        if(salesperson != 'D' && salesperson != 'E' && salesperson != 'F') { // Validate input of salespersons initials
             printf("    Intermediate Output: Error, Invalid Salesperson Selected, Please Try Again\n");
             continue;
         }
 
-        printf("Sales ($): ");
+        printf("Sales ($): "); // Validate input of sales_amount 
         if (scanf("%lf", &sales_amount) != 1 || sales_amount < 0) {  
             printf("    Intermediate Output: Error, Invalid Sale's Amount, Please Try Again\n");
-            while (getchar() != '\n');
+            while (getchar() != '\n'); // Clear input buffer
             continue;
         }
 
-        if (salesperson == 'D') {
+        if (salesperson == 'D') { // Add sales_amount to each corresponding salesperson
             sales_D += sales_amount;
         } else if (salesperson == 'E') {
             sales_E += sales_amount;
@@ -36,15 +36,15 @@ int main(){
         }
     }
 
-    printf("\nEach Person's Total Sales:\n");
+    printf("\nEach Person's Total Sales:\n"); // Display each salespersons sales_amount 
     printf("D: $%d\n", sales_D);
     printf("E: $%d\n", sales_E);
     printf("F: $%d\n", sales_F);
 
-    int grand_total = sales_D + sales_E + sales_F;
+    int grand_total = sales_D + sales_E + sales_F; // GrandTotal
     printf("\nGrand Total: $%d\n", grand_total);
 
-    int highest_sales = (sales_D > sales_E) ? ((sales_D > sales_F) ? sales_D : sales_F) : ((sales_E > sales_F) ? sales_E : sales_F);
+    int highest_sales = (sales_D > sales_E) ? ((sales_D > sales_F) ? sales_D : sales_F) : ((sales_E > sales_F) ? sales_E : sales_F); // Compare/Determine Highest Sales
     printf("Highest Sale: ");
     if (sales_D == highest_sales) printf("Danielle ");
     if (sales_E == highest_sales) printf("Edward ");
