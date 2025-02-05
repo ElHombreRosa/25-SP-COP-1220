cdff87295979c55f3abbd7d5d4b1f15d6cb958d5
ElHombreRosa <gabrielrosado0326@gmail.com>
2025-02-05 13:08:32 -0500

    HomeSales Assignment Work in Progress

---
+++ Module 2/Assignment 2-4/HomeSales.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int sales_D = 0, sales_E = 0, sales_F = 0;
+    double sales_amount;
+    char salesperson;
+
+    while(1) {
+        printf("Sales Person Initials: (D, E, F, & Z To End Application): ");
+        scanf(" %c", &salesperson);
+        salesperson = toupper(salesperson);
+
+        if (salesperson == 'Z'){
+            break;
+        }
+
+        if(salesperson != 'D' && salesperson != 'E' && salesperson != 'F') {
+            printf("    Intermediate Output: Error, Invalid Salesperson Selected, Please Try Again\n");
+            continue;
+        }
+
+        printf("Sales ($): ");
+        if (scanf("%lf", &sales_amount) != 1 || sales_amount < 0) {  
+            printf("    Intermediate Output: Error, Invalid Sale's Amount, Please Try Again\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        if (salesperson == 'D') {
+            sales_D += sales_amount;
+        } else if (salesperson == 'E') {
+            sales_E += sales_amount;
+        } else if (salesperson == 'F'){
+            sales_F += sales_amount;
+        }
+    }
+
+    printf("Each Person's Total Sales:\n");
+    printf("D: $%d\n", sales_D);
+    printf("E: $%d\n", sales_E);
+    printf("F: $%d\n", sales_F);
+
+    int grand_total = sales_D + sales_E + sales_F;
+    printf("Grand Total: $%d\n", grand_total);
+
+
+    return 0; 
+}
