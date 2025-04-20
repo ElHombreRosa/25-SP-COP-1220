6867af8fad2b44c36251da87987e65c89c7b8f93
ElHombreRosa <gabrielrosado0326@gmail.com>
2025-04-20 14:16:09 -0400

    Notes structs

---
+++ Notes/Notes4202025/structs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Person {
+    char firstname[50];
+    char lastname[50];
+    int age;
+    int tenure;
+};
+
+int main(void){
+    struct Person student =
+    {
+        "John",
+        "Smith",
+        52,
+        15
+    };
+    
+
+    printf("Structure Data - \n"
+            "*****************\n"
+            "First Name: %s\n"
+            "LastName: %s\n"
+            "Age: %d\n"
+            "Tenure: %d yrs\n",student.firstname, student.lastname,
+            student.age, student.tenure);
+
+    return 0;
+}
